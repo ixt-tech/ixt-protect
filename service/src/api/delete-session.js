897d7002ea5126afbaf52c7cf622166d0c2c71cb
Ingemar Svensson <ingemar.svensson@ixledger.com>
2019-01-23 06:45:10 +0000

    handlers

--- service/src/api/delete-session.js
+++ service/src/api/delete-session.js
@@ -1,16 +1,11 @@
 'use strict';
 
-const getConnection = require('../db/pool');
-
 module.exports.handler = async (event, context) => {
-
-  const connection = getConnection();
-  console.log(connection);
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Sign up saved successfully',
+      message: 'Success',
       input: event,
     }),
   };
--- service/src/db/pool.js
+++ service/src/db/pool.js
@@ -2,16 +2,16 @@ const mysql = require('mysql2');
 const config = require('./config');
 
 const pool = mysql.createPool({
-    connectionLimit: 1,
+    connectionLimit: 10,
     host: config.MYSQL_HOST,
     port: config.MYSQL_PORT,
     database: config.MYSQL_DATABASE,
     user: config.MYSQL_USER,
     password: config.MYSQL_PASSWORD,
-    multipleStatements: true,
-    connectTimeout: 2000
+    multipleStatements: false,
+    connectTimeout: 5000
 });
 
 module.exports = getConnection = async () => {
-  return pool.getConnection();
+  return await pool.getConnection();
 }
